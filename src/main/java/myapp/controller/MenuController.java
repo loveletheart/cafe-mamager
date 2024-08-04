a6822acbd000a9551fbd2c56365e9e61c1712380
zsx1397 <zsx1397@DESKTOP-I407T1R>
2024-08-05 01:04:18 +0900

    1.html수정 ex)커피 페이지로 진입시 해당 페이지 맞는 태그 버튼 색상 변경 2.컨트롤러 수정 컨트롤러 부분 간소화 및 공통 기능 분리 및 추가

--- src/main/java/myapp/controller/MenuController.java
+++ src/main/java/myapp/controller/MenuController.java
@@ -51,8 +51,8 @@ public class MenuController {
     private ModelAndView getMenu(String category) {
         List<Menu> menus = menuService.getMenuByValue(category);// 데이터베이스에서 category라는 조건으로 검색
         ModelAndView modelAndView = new ModelAndView(category);
-        modelAndView.addObject("menus", menus);
-        modelAndView.addObject("activeCategory", category);
+        modelAndView.addObject("menus", menus);//menus에 맞는 데이터 저장
+        modelAndView.addObject("activeCategory", category);//카테고리에 맞는 버튼 색상 뒷배경과 통일
         return modelAndView;
     }
 }
