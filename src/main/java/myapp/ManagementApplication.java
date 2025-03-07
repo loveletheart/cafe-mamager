81977bc95537e1696db2153a5e839fead68a07cd
loveletheart <zsx1397@gmail.com>
2025-03-07 20:19:52 +0900

    1.git테스트

--- src/main/java/myapp/ManagementApplication.java
+++ src/main/java/myapp/ManagementApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ManagementApplication {
-
+	//git 테스트
 	public static void main(String[] args) {
 		SpringApplication.run(ManagementApplication.class, args);
 	}
