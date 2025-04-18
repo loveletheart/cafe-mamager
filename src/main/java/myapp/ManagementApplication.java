c3c24d8bcae2c76cc770f30de2f875175a3123f6
zsx1397 <zsx1397@DESKTOP-8L563RJ>
2025-04-18 11:07:10 +0900

    1.다시 테스트

--- src/main/java/myapp/ManagementApplication.java
+++ src/main/java/myapp/ManagementApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ManagementApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(ManagementApplication.class, args);//aws자동업데이트 테스트
+		SpringApplication.run(ManagementApplication.class, args);//aws자동업데이트 테스트1
 	}
 
 }
