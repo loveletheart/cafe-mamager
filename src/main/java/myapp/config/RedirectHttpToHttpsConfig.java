9f5bb3f7f0f45e7ae7a5ec48af501d8928de01ff
zsx1397 <zsx1397@DESKTOP-8L563RJ>
2025-04-19 10:29:10 +0900

    1.8080포트랑 8081포트로 접속시 자동으로 8443로 넘어오겠끔 변경

--- src/main/java/myapp/config/RedirectHttpToHttpsConfig.java
+++ src/main/java/myapp/config/RedirectHttpToHttpsConfig.java
@@ -1,24 +1,33 @@
 package myapp.config;
 
+import org.apache.catalina.connector.Connector;
+import org.apache.coyote.http11.Http11NioProtocol;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServerFactoryCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.apache.catalina.connector.Connector;
-import org.apache.coyote.http11.Http11NioProtocol;
 
 @Configuration
 public class RedirectHttpToHttpsConfig {
 
     @Bean
-    WebServerFactoryCustomizer<TomcatServletWebServerFactory> servletContainer() {
+    public WebServerFactoryCustomizer<TomcatServletWebServerFactory> servletContainer() {
         return factory -> {
-            Connector connector = new Connector(Http11NioProtocol.class.getName());
-            connector.setScheme("http");
-            connector.setPort(8081); // HTTP 포트
-            connector.setSecure(false);
-            connector.setRedirectPort(8081); // HTTPS 포트로 리디렉션
-            factory.addAdditionalTomcatConnectors(connector);
+            // 8080 포트용 커넥터
+            Connector connector1 = new Connector(Http11NioProtocol.class.getName());
+            connector1.setScheme("http");
+            connector1.setPort(8080);
+            connector1.setSecure(false);
+            connector1.setRedirectPort(8443); // HTTPS로 리디렉션
+
+            // 8081 포트용 커넥터
+            Connector connector2 = new Connector(Http11NioProtocol.class.getName());
+            connector2.setScheme("http");
+            connector2.setPort(8081);
+            connector2.setSecure(false);
+            connector2.setRedirectPort(8443); // HTTPS로 리디렉션
+
+            factory.addAdditionalTomcatConnectors(connector1, connector2);
         };
     }
 }
